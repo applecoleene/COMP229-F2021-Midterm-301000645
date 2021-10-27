3a3612ecb895749366a6ae37782a39d76961764c
applecoleene <90641429+applecoleene@users.noreply.github.com>
2021-10-27 00:50:46 -0400

    edited internal documentation

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ File name: index.js
 Author’s Name: Apple Coleene Duquinal
 StudentID:  301000645
 Web App Name: COMP229-M2021-MidTerm-301000645
-*/
+*/  
 
 exports.home = function(req, res, next) {
     res.render('index', { title: 'Home' });
